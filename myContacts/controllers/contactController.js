d87b8463d77947f110b249f3351ebfc6f56a6b35
ninano1330 <ninano1330@gmail.com>
2026-01-14 23:37:44 +0900

    몽고DB CRUD

---
+++ myContacts/controllers/contactController.js
@@ -0,0 +1,62 @@
+const asyncHandler = require('express-async-handler'); // 에러 체크 모듈에게 맡김
+const Contact = require('../models/contactModel');
+
+const getAllContacts = asyncHandler(async(req, res) => {
+    const contacts = await Contact.find();
+    res.send(contacts);
+});
+
+const createContact = asyncHandler(async(req, res) => {
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone) {
+        return res.send('필수 값이 입력되지 않았습니다.');
+    }
+
+    const contact = await Contact.create({
+        name, email, phone
+    });
+
+    res.send('Create Contacts');
+});
+
+const getContact = asyncHandler(async(req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    res.send(contact);
+});
+
+const updateContact = asyncHandler(async(req, res) => {
+    const id = req.params.id;
+    const {name, email, phone} = req.body;
+    const contact = await Contact.findById(id);
+
+    if(!contact) {
+        throw new Error('Contact not found.');
+    }
+
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+    contact.save();
+
+    res.json(contact);
+});
+
+const deleteContact = asyncHandler(async(req, res) => {
+    const id = req.params.id;
+    const contact = await Contact.findById(id);
+
+    if(!contact) {
+        throw new Error('Contact not found.');
+    }
+
+    await Contact.deleteOne();
+    res.send('Deleted');
+});
+
+module.exports = {
+    getAllContacts, 
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+}; 
---
+++ myContacts/models/contactModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+        required: [true, '전화번호는 꼭 기입해 주세요.']
+    }
+},
+{
+    timestamps: true // 시간 기록
+});
+
+// 스키마 -> 모델
+// mongoose.model(모델명, 스키마명)
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
